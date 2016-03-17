10a17228974093f462faa8ca398de1f6ed9b166d
chinesedfan <chinesedfan@qq.com>
2016-03-17 13:43:17 +0800

    support options.inputList to preset input

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var solve = require('./lib');
 var args = process.argv.slice(2);
 
 if (args.length < 1) {
-	console.log('Useage: node ' + path.basename(__filename) + ' <input>');
+	console.log('Useage: node ' + path.basename(__filename) + ' <bin_file> [input_file]');
 	return;
 }
 
 fs.readFile(args[0], function(err, content) {
 	if (err) throw err;
 
-	solve(content);
+    var inputList;
+    if (args.length >= 2) {
+        inputList = fs.readFileSync(args[1]).toString().split('\n');
+    }
+	solve(content, {
+        inputList: inputList
+    });
 });
