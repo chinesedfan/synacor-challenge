0609542d884c1ccbba863982174296162b4f20c0
chinesedfan <chinesedfan@qq.com>
2016-03-20 15:33:15 +0800

    optimize as lifecycle

--- tools/r7solver.js
+++ tools/r7solver.js
@@ -11,49 +11,60 @@ console.log('...done with:', r0, r1, r7);
 
 function call6027() {
     var key = getKey();
-    if (typeof cache[key] !== 'undefined') {
-        r0 = cache[key].r0;
-        r1 = cache[key].r1;
-        retDebug('%s returns %d %d', key, r0, r1);
-        return;
-    }
+    if (onCallStart(key) === false) return;
 
     if (!r0) {
         r0 = r1 + 1;
-        retDebug('%s returns %d %d', key, r0, r1);
-        updateCache(key);
+        onCallEnd(key);
         return;
     }
 p6035:
     if (!r1) {
         r0 = r0 - 1;
         r1 = r7;
-        callDebug('%s calls %s', key, getKey());
+        beforeCallSub(key);
         call6027();
-        retDebug('%s returns %d %d', key, r0, r1);
-        updateCache(key);
+        onCallEnd(key);
         return;
     }
 p6048:
     stack.push(r0);
     r1 = r1 - 1;
-    callDebug('%s calls %s', key, getKey());
+    beforeCallSub(key);
     call6027();
     r1 = r0;
     r0 = stack.pop();
     r0 = r0 - 1;
-    callDebug('%s calls %s', key, getKey());
+    beforeCallSub(key);
     call6027();
-    retDebug('%s returns %d %d', key, r0, r1);
-    updateCache(key);
+    onCallEnd(key);
     return;
 }
 function getKey() {
     return r0 + '-' + r1;
 }
-function updateCache(key) {
-    cache[key] = {
-        r0: r0,
-        r1: r1
-    };
+function onCallStart(key) {
+    if (typeof cache[key] === 'undefined') {
+        cache[key] = {};
+    } else {
+        r0 = cache[key].r0;
+        r1 = cache[key].r1;
+        onCallEnd(key);
+        return false;
+    }
+}
+function onCallEnd(key) {
+    cache[key].r0 = r0;
+    cache[key].r1 = r1;
+
+    retDebug('%s returns %d %d', key, r0, r1);
+}
+function beforeCallSub(key) {
+    var subkey = getKey();
+    if (!cache[key].children) {
+        cache[key].children = [];
+    } 
+    cache[key].children.push(subkey);
+
+    callDebug('%s calls %s', key, subkey);
 }
