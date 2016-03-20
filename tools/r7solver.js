1be5bcd1bed88726ea388f30ee3aaf43555cda4c
chinesedfan <chinesedfan@qq.com>
2016-03-20 12:47:16 +0800

    add cache

--- tools/r7solver.js
+++ tools/r7solver.js
@@ -4,15 +4,24 @@ var retDebug = debug('ret');
 
 var r0 = 4, r1 = 1, r7 = 0;
 var stack = [];
+var cache = [];
 
 call6027();
 console.log('...done with:', r0, r1, r7);
 
 function call6027() {
     var key = getKey();
+    if (typeof cache[key] !== 'undefined') {
+        r0 = cache[key].r0;
+        r1 = cache[key].r1;
+        retDebug('%s returns %d %d', key, r0, r1);
+        return;
+    }
+
     if (!r0) {
         r0 = r1 + 1;
         retDebug('%s returns %d %d', key, r0, r1);
+        updateCache(key);
         return;
     }
 p6035:
@@ -22,6 +31,7 @@ p6035:
         callDebug('%s calls %s', key, getKey());
         call6027();
         retDebug('%s returns %d %d', key, r0, r1);
+        updateCache(key);
         return;
     }
 p6048:
@@ -35,8 +45,15 @@ p6048:
     callDebug('%s calls %s', key, getKey());
     call6027();
     retDebug('%s returns %d %d', key, r0, r1);
+    updateCache(key);
     return;
 }
 function getKey() {
     return r0 + '-' + r1;
+}
+function updateCache(key) {
+    cache[key] = {
+        r0: r0,
+        r1: r1
+    };
 }
